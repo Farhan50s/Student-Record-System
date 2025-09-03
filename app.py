b1abdcb45009dec0fe781763774d0659e3687045
Farhan50s <mf0224530@gmail.com>
2025-09-03 21:35:13 +0500

    Some additions

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 from flask import Flask ,render_template
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.String(20), unique=True, nullable=False)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    date_of_birth = db.Column(db.Date, nullable=False)
+    major = db.Column(db.String(100))
+    enrollment_date = db.Column(db.Date, nullable=False)
+    graduation_date = db.Column(db.Date)
+    gpa = db.Column(db.Float)
+    
+    def __repr__(self):
+        return f'<Student {self.first_name} {self.last_name}>'
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
